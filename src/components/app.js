c8fc0d22e63d3fdf5addbc12f988e476308cd23b
Klinton Ballecer <klinton.ballecer@gmail.com>
2018-01-09 12:51:46 +0800

    :tada: Initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,8 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 
-import Header from './header';
 import Home from '../routes/home';
-import Profile from '../routes/profile';
 // import Home from 'async!../routes/home';
-// import Profile from 'async!../routes/profile';
 
 export default class App extends Component {
 	/** Gets fired when the route changes.
@@ -19,11 +16,8 @@ export default class App extends Component {
 	render() {
 		return (
 			<div id="app">
-				<Header />
 				<Router onChange={this.handleRoute}>
 					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
 				</Router>
 			</div>
 		);
